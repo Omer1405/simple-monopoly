48a2881a4e0f10657c0e002ecaf781c4dd610813
Omer1405 <butt.omer-it24@it.dhbw-ravensburg.de>
2026-02-13 15:15:42 +0100

    ka

--- src/ui.js
+++ src/ui.js
@@ -1,50 +1,4 @@
 class UI {
-<<<<<<< HEAD
-    constructor(canvasId) {
-        this.canvas = document.getElementById(canvasId);
-        this.context = this.canvas.getContext('2d');
-        this.players = [];
-        this.dice = [1, 1];
-        this.currentTurn = 0;
-        this.boardFields = this.createBoardFields();
-        this.render();
-    }
-
-    createBoardFields() {
-        const fields = [];
-        for (let i = 0; i < 40; i++) {
-            fields.push({ id: i, x: 0, y: 0, name: `Field ${i + 1}` });
-        }
-        return fields;
-   }
-
-    addPlayer(tokenColor) {
-        this.players.push({ color: tokenColor, position: 0 });
-        this.renderPlayer({ color: tokenColor, position: 0 });
-    }
-
-    updatePlayerInfo(playerIndex, newPosition) {
-        this.players[playerIndex].position = newPosition;
-        this.render();
-    }
-
-    rollDice() {
-        this.dice = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
-        this.updateDiceDisplay();
-        this.nextTurn();
-    }
-
-    updateDiceDisplay() {
-        // Display dice values
-    }
-
-    showMessage(message) {
-        // Show messages on UI
-    }
-
-    render() {
-        this.clearCanvas();
-=======
     constructor() {
         this.board = [];
         this.message = '';
@@ -52,7 +6,6 @@ class UI {
 
     init() {
         // Initialize the UI components
->>>>>>> 6f605e941f293e48320b346cd72c3390be2e90bd
         this.drawBoard();
     }
 
@@ -66,10 +19,6 @@ class UI {
         this.message = msg;
         console.log(this.message);
     }
-<<<<<<< HEAD
-}
-=======
 }
 
 export default UI;
->>>>>>> 6f605e941f293e48320b346cd72c3390be2e90bd
